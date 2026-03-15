f143a244907da953ab7ca208e8042e0ea0e5796e
Menaga-J-711 <jmenaga7112004@gmail.com>
2026-03-15 18:46:41 +0530

    Add TLS configuration to email transporter

--- newProject/Backend/routes/otp.js
+++ newProject/Backend/routes/otp.js
@@ -56,6 +56,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
   }
+  tls: {
+  rejectUnauthorized: false
+}
 });
 
     await transporter.sendMail({
