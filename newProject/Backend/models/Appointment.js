b370b047c4258e07d5af61630063627bb92bb4c3
Menaga-J-711 <jmenaga7112004@gmail.com>
2026-03-26 21:54:39 +0530

    Add timestamps to Appointment schema

--- newProject/Backend/models/Appointment.js
+++ newProject/Backend/models/Appointment.js
@@ -4,6 +4,7 @@ const AppointmentSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true },
+    { timestamps: true },
 
     doctor: {
       type: mongoose.Schema.Types.ObjectId,
